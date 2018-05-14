2a2545cd7471e6848397cce75b95f9da9fba6d7a
Alex Ignatenko <ignatenko2207@gmail.com>
2018-05-14 21:00:48 +0300

    Initial commit

---
+++ src/it/dan/ApplicationRunner.java
@@ -0,0 +1,9 @@
+package it.dan;
+
+public class ApplicationRunner
+{
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/it/dan/dao/ClientDAO.java
@@ -0,0 +1,29 @@
+package it.dan.dao;
+
+import it.dan.model.Client;
+
+public class ClientDAO
+{
+
+	public static void save(Client client)
+	{
+
+	}
+
+	public static Client get(String login)
+	{
+
+		return null;
+	}
+
+	public static void update(Client client)
+	{
+
+	}
+
+	public static void delete(String login)
+	{
+
+	}
+
+}
