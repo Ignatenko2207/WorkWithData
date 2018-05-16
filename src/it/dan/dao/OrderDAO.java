7774ef44507265241962e84565b26c48beb1aa60
Alex Ignatenko <alex.ignatenko@scalors.com>
2018-05-16 21:24:40 +0300

    Add to OrderDAO getByClient method

--- src/it/dan/dao/OrderDAO.java
+++ src/it/dan/dao/OrderDAO.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import com.sun.org.apache.xpath.internal.operations.Or;
 import it.dan.model.Order;
 
 public class OrderDAO extends AbstractDAO<Order>
@@ -11,7 +14,7 @@ public class OrderDAO extends AbstractDAO<Order>
 	@Override
 	public void save(Order order)
 	{
-		String sql = "INSERT INTO order(client_id, item_id, amount) VALUES(?,?,?)";
+		String sql = "INSERT INTO public.order(client_id, item_id, amount) VALUES(?,?,?)";
 
 		try ( Connection connection = ConnectionToDB.getConnection(); PreparedStatement statement = connection.prepareStatement(sql); )
 		{
@@ -46,11 +49,12 @@ public class OrderDAO extends AbstractDAO<Order>
 		}
 	}
 
-	@Override public Order get(Object orderId)
+	@Override
+	public Order get(Object orderId)
 	{
 		Order order = new Order();
 
-		String sql = "SELECT * FROM order WHERE order_id='" + orderId + "'";
+		String sql = "SELECT * FROM public.order WHERE order_id='" + orderId + "'";
 
 		try (
 			Connection        connection  = ConnectionToDB.getConnection();
@@ -75,9 +79,40 @@ public class OrderDAO extends AbstractDAO<Order>
 		return null;
 	}
 
+	public List<Order> getByClient(String clientId)
+	{
+		List<Order> orders = new ArrayList<>();
+
+		String sql = "SELECT * FROM public.order WHERE client_id='" + clientId + "'";
+
+		try (
+			Connection        connection  = ConnectionToDB.getConnection();
+			PreparedStatement statement  = connection.prepareStatement(sql);
+			ResultSet rSet = statement.executeQuery();
+		)
+		{
+			while ( rSet.next() )
+			{
+				Order order = new Order();
+
+				order.setOrderId(rSet.getInt("order_id"));
+				order.setClientId(rSet.getString("client_id"));
+				order.setItemId(rSet.getString("item_id"));
+				order.setAmount(rSet.getInt("amount"));
+
+				orders.add(order);
+			}
+		}
+		catch ( SQLException e )
+		{
+			e.printStackTrace();
+		}
+		return orders;
+	}
+
 	@Override public void delete(Object orderId)
 	{
-		String sql = "DELETE * FROM order WHERE order_id=?";
+		String sql = "DELETE * FROM public.order WHERE order_id=?";
 
 		try (
 			Connection connection = ConnectionToDB.getConnection();
